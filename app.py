c7d94733d1afa6f98fb825e077ab359591e6b837
Aashik Maharjan <amaharjan@bcicorp.com>
2025-10-07 10:46:39 +0545

    add travis ci file

---
+++ app.py
@@ -0,0 +1,7 @@
+import requests
+import numpy as np
+import pandas as pd
+
+print("Requests version:", requests.__version__)
+print("Numpy version:", np.__version__)
+print("Pandas version:", pd.__version__)
